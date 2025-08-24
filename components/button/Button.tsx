8e81d5ba7c561e0cc27389c4caf294015156e78b
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2025-08-24 21:16:45 +0200

    componentLab; styled buttons

---
+++ components/button/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface ButtonProps {
+    type: "primary" | "secondary" | "tertiary",
+    text: string
+}
+
+const typeClassMap: Record<ButtonProps["type"], string> = {
+    primary: "bg-primaryBtn text-primaryBtnTxt",
+    secondary: "bg-transparent border-secondaryBtn border text-secondaryBtn",
+    tertiary: "bg-transparent border-tertiaryBtn border text-tertiaryBtn"
+};
+
+function getButtonClass(type: ButtonProps["type"]) {
+    return `${typeClassMap[type]} px-2.5 py-1.5 rounded-md text-base`;
+}         
+ 
+export default function Button({ type, text }: ButtonProps) {
+    return (
+        <button className={getButtonClass(type)} >{text}</ button>
+    )
+}
