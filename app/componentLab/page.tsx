e82bfd5215851eb0a49b7dbee9842c0608e80f14
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2025-08-26 19:53:05 +0200

    button component variants according to Figma

--- app/componentLab/page.tsx
+++ app/componentLab/page.tsx
@@ -5,15 +5,20 @@ import CarouselButton from "@/components/carouselButton/CarouselButton";
 import CategoryFilter from "@/components/categoryFilter/CategoryFilter";
 import Checkbox from "@/components/checkbox/Checkbox";
 import DotIndicator from "@/components/dotIndicator/DotIndicator";
+import { PlusSign } from "@/components/icons/icons";
 import ProductTile from "@/components/productTile/ProductTile";
 
 export default function Home() {
   return (
     <>
-      <Button type="primary" >Primary</Button>
-      <Button type="secondary" >Secondary</Button>
+      <Button className="btn btn-tertiary btn-xs">
+        <PlusSign />
+        Primary
+        <PlusSign />
+      </Button>
+      {/* <Button type="secondary" >Secondary</Button>
       <Button type="tertiary" >Tertiary</Button>
-      <BrandCategoryButton text="Mouse"></BrandCategoryButton>
+      <BrandCategoryButton text="Mouse"></BrandCategoryButton> */}
       {/* <BrandCategoryButton text="Monitor"></BrandCategoryButton>
       <BrandCategoryButton text="Headphones"></BrandCategoryButton>
       <BrandCategoryButton text="Keyboard"></BrandCategoryButton>
@@ -23,7 +28,7 @@ export default function Home() {
       <DotIndicator dots={5} active={1} /> */}
       {/* <Carousel /> */}
       {/* <ProductTile product={{ productName: "Logitech G502 hero", category: "Mouse", price: 52.99, oldPrice: 72.99 }} /> */}
-      <CategoryFilter />
+      {/* <CategoryFilter /> */}
       {/* <Checkbox /> */}
     </>
   );
--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 
 interface ButtonProps {
-    type: "primary" | "secondary" | "tertiary",
     children: React.ReactNode,
     className?: string,
+    disabled?: true,
     onClick?: () => void
 }
 
-const typeClassMap: Record<ButtonProps["type"], string> = {
-    primary: "bg-primaryBtn text-primaryBtnTxt",
-    secondary: "bg-transparent border-secondaryBtn border text-secondaryBtn",
-    tertiary: "bg-transparent border-tertiaryBtn border text-tertiaryBtn"
-};
 
-function getButtonClass(type: ButtonProps["type"], className?: string) {
-    return `${typeClassMap[type]} px-[20px] rounded-md text-base flex gap-[14px] items-center ${className || ""}`;
-}
 
-const Button = ({ type, children, className }: ButtonProps) => {
-    return (
-        <button className={getButtonClass(type, className)} >{children}</ button>
+const Button = ({ children, className, disabled }: ButtonProps) => {
+    return (<button className={className} disabled={disabled}>{children}</button>
     )
 }
 
